10fb8b39f6442b31f4d30efd09777f5768a8a861
Rubí <ramseymegan17@gmail.com>
2024-08-02 13:15:07 -0500

    Create footer.php

---
+++ VN LA/footer.php
@@ -0,0 +1,68 @@
+<!-- footer.php -->
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=swap" rel="stylesheet">
+    <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
+    <link href="./img/VIENA LOGO 1.png" rel="icon">
+    <link href="./img/VIENA LOGO 1.png" rel="apple-touch-icon">
+    <link rel="stylesheet" href="estilos/footer.css">
+<footer class="pie">
+  <div class="header">
+    <div class="logo">
+      <img src="./img/viena logo 1.png" alt="">
+      <p class="logotipo">VIENA INGENIEROS</p>
+    </div>
+    <div class="redes">
+      <span class="fb"><a href="https://www.facebook.com/share/p/km9i8R5i9HFm7g9F/" target="_blank"><i class="fab fa-facebook"></i></a></span>
+      <span class="tw"><a href="https://twitter.com/tupagina" target="_blank"><i class="fab fa-twitter"></i></a></span>
+      <span class="in"><a href="https://www.instagram.com/vienaingenieros?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank"><i class="fab fa-instagram"></i></a></span>
+      <span class="li"><a href="https://www.tiktok.com/company/tupagina" target="_blank"><i class="fab fa-tiktok"></i></a></span>
+      <span class="yt"><a href="https://www.youtube.com/user/tupagina" target="_blank"><i class="fab fa-youtube"></i></a></span>
+    </div>
+  </div>
+  <hr>
+  <div class="section">
+    <div>
+      <h2>Company</h2>
+      <a href="#">Home</a>
+      <a href="#">Contact Us</a>
+      <a href="#">About Us</a>
+      <a href="#">Get Started</a>
+    </div>
+    <div>
+      <h2>Ubicación</h2>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d957.0075613202561!2d-71.54509783051022!3d-16.37242161229889!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91424a3cc1db762d%3A0x87023f3332c63b38!2sAv.%20Bolognesi%20900%2C%20Arequipa%2004017!5e0!3m2!1ses-419!2spe!4v1721662250130!5m2!1ses-419!2spe" width="250" height="150" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+      <p>Calle Principal 123, Arequipa</p>
+    </div>
+    <div>
+      <h2>Services</h2>
+      <a href="#">App Design</a>
+      <a href="#">Web Design</a>
+      <a href="#">Logo Design</a>
+      <a href="#">Banner Design</a>
+    </div>
+    <div>
+      <h2>Subscribe</h2>
+      <a href="https://wa.me/+51962945890?text=Hola%2C%20quisiera%20pedir%20mas%20informacion%21" class="whatsapp-button" target="_blank">
+        <i class="fab fa-whatsapp"></i>
+      </a>
+      <form action="">
+        <input type="text" placeholder="Enter your Email">
+        <button type="submit">Subscribe</button>
+      </form>
+    </div>
+  </div>
+  <div class="derechos">
+    <p>Copyright©2024 VIENA INGENIEROS. All right reserved</p>
+    <p>Privacy Policy | Terms and Conditions</p>
+  </div>
+</footer>
