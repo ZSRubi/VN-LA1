1d43e36effaeeb795364d55913a1861dd984e4d9
CatApaza <taniaapaza2006@gmail.com>
2024-08-05 10:22:32 -0500

    Update proyectos.php

---
+++ VN LA/proyectos.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-sacalable=no, initial-scale=1.0, maximun-scale =1.0,minimun-scale=1.0">
+    <link rel="stylesheet" href="estilos/proyectos.css">
+    <title>VIENA INGENIEROS</title>
+
+  <!-- Favicons -->
+  <link href="./image/VIENA LOGO 1.png" rel="icon">
+  <link href="./image/VIENA LOGO 1.png" rel="apple-touch-icon">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sofia+Sans+Semi+Condensed:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer"
+    />
+
+
+  <!-- Google Fonts -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+</head>
+
+
+
+<body>
+
+    <div class="slider">
+        <div class="list">
+
+            <div class="item">
+                <img src="./image/imagen01.jpg" alt="">
+
+                <div class="content">
+                    <div class="title">VIENA INGENIEROS</div>
+                    <div class="type"><h1>SAC</h1></div>
+                    <div class="description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti temporibus quis eum consequuntur voluptate quae doloribus distinctio. Possimus, sed recusandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, aut.
+                    </div>
+                    <div class="button">
+                        <button>MAS</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="./image/imagen02.jpg" alt="">
+
+                <div class="content">
+                    <div class="title">MAGIC SLIDER</div>
+                    <div class="type">NATURE</div>
+                    <div class="description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti temporibus quis eum consequuntur voluptate quae doloribus distinctio. Possimus, sed recusandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, aut.
+                    </div>
+                    <div class="button">
+                        <button>MAS</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="./image/imagen03.jpg" alt="">
+
+                <div class="content">
+                    <div class="title">MAGIC SLIDER</div>
+                    <div class="type">PLANT</div>
+                    <div class="description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti temporibus quis eum consequuntur voluptate quae doloribus distinctio. Possimus, sed recusandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, aut.
+                    </div>
+                    <div class="button">
+                        <button>MAS</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="./image/imagen04.png" alt="">
+
+                <div class="content">
+                    <div class="title">MAGIC SLIDER</div>
+                    <div class="type">NATURE</div>
+                    <div class="description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti temporibus quis eum consequuntur voluptate quae doloribus distinctio. Possimus, sed recusandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, aut.
+                    </div>
+                    <div class="button">
+                        <button>MAS</button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+        <div class="thumbnail">
+
+            <div class="item">
+                <img src="./image/imagen01.jpg" alt="">
+            </div>
+            <div class="item">
+                <img src="./image/imagen02.jpg" alt="">
+            </div>
+            <div class="item">
+                <img src="./image/imagen03.jpg" alt="">
+            </div>
+            <div class="item">
+                <img src="./image/imagen04.png" alt="">
+            </div>
+
+        </div>
+
+
+        <div class="nextPrevArrows">
+            <button class="prev"> < </button>
+            <button class="next"> > </button>
+        </div>
+
+
+    </div>
+
+ 
+   
+    <!-- Más scripts según tus necesidades -->
+    <script src="slyder.js"></script>
+</body>
+</html>
